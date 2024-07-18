0ed06249d8198b48be4f727243bb17d8d5a16e7f
Lucas Levassor <lucaslevassor@gmail.com>
2024-07-18 14:57:03 +0200

    wip: added input logic to choose regions and automatically annotate files.

--- streptocad/sequence_loading/sequence_loading.py
+++ streptocad/sequence_loading/sequence_loading.py
@@ -1,9 +1,11 @@
 import os
-from typing import List
+from typing import List, Dict
 from Bio import SeqIO
 from pydna.dseqrecord import Dseqrecord
 from teemi.design.fetch_sequences import read_fasta_files, read_genbank_files
-
+from Bio.SeqFeature import SeqFeature, FeatureLocation
+import re
+import tempfile
 
 def load_and_process_genome_sequences(path_to_file: str) -> List[Dseqrecord]:
     """
@@ -84,7 +86,6 @@ def load_and_process_plasmid(path_to_file: str) -> List[Dseqrecord]:
     return clean_plasmid
 
 
-
 def load_and_process_gene_sequences(path_to_genome: str) -> dict:
     """
     Load and process gene sequences from a GenBank or FASTA file.
@@ -110,3 +111,60 @@ def load_and_process_gene_sequences(path_to_genome: str) -> dict:
                     gene_sequences[locus_tag] = gene_seq
                     
     return gene_sequences
+
+
+
+def check_and_convert_input(input_list):
+    def convert_input_to_dict(input_list):
+        target_dict = []
+        target_keys = []
+        for index, item in enumerate(input_list):
+            positions = item.split('-')
+            
+            # Ensure the length of positions is even and contains exactly two numbers
+            assert len(positions) == 2, f"Input format error: {item} should contain exactly two numbers separated by a dash."
+            
+            # Ensure all positions are numbers
+            for pos in positions:
+                assert pos.isdigit(), f"Input format error: {pos} is not a valid number."
+            
+            # Convert positions to integers
+            start, end = int(positions[0]), int(positions[1])
+            
+            # Ensure the second number is not smaller than the first number
+            assert start <= end, f"Input format error: the second number {end} is smaller than the first number {start} in {item}."
+            
+            # Create the dictionary for this region
+            region_key = f'target_region_{index + 1}'
+            region_dict = {region_key: [start, end]}
+            
+            # Add the dictionary to the list
+            target_keys.append(region_key)
+            target_dict.append(region_dict)
+        
+        return target_dict, target_keys, True
+
+    # Check if the input is a list of ranges
+    if all(re.fullmatch(r"\d+-\d+", item) for item in input_list):
+        return convert_input_to_dict(input_list)
+    
+    # Check if the input is a list of gene names
+    elif all(re.fullmatch(r"[A-Za-z0-9_]+", item) for item in input_list):
+        return input_list, input_list,  False
+    
+    # Raise an assertion error if the input format is incorrect
+    else:
+        raise AssertionError("Input format error: The input should be either a list of ranges or a list of gene names.")
+
+
+def annotate_dseqrecord(dseqrecord, target_dict):
+    for annotation in target_dict:
+        for region, positions in annotation.items():
+            start, end = positions
+            
+            feature_location = FeatureLocation(start, end, strand=1)
+            feature = SeqFeature(feature_location, type="CDS", qualifiers={ "locus_tag": region})
+            dseqrecord.features.append(feature)
+    
+    return dseqrecord
+
