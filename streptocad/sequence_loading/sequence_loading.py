0368a7a89962b5c54bf3a0d121a2db8ee91470f6
Lucas Levassor <lucaslevassor@gmail.com>
2024-07-25 10:07:56 +0200

    wip: new naming of regions

--- streptocad/sequence_loading/sequence_loading.py
+++ streptocad/sequence_loading/sequence_loading.py
@@ -175,7 +175,7 @@ def convert_to_dict(input_list):
         start, end = validate_range_format(item)
         
         # Create the dictionary for this region
-        region_key = f'region_{index + 1}_({start}-{end})'
+        region_key = f'region{index + 1}_({start}-{end})'
         region_dict = {region_key: [start, end]}
         
         # Add the dictionary to the list
