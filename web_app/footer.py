3312f19deb2528e594a27772e4b5434a997e0f06
Lucas Levassor <lucaslevassor@gmail.com>
2024-08-12 09:43:39 +0200

    wip: bigger dtu logo

--- web_app/footer.py
+++ web_app/footer.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 # Footer content
 footer_content = html.Footer([
     html.Div([
-        html.Img(src="/assets/dtu_logo.png", style={"height": "50px", "marginRight": "20px"}),  # DTU logo
+        html.Img(src="/assets/dtu_logo.png", style={"height": "100px", "marginRight": "20px"}),  # DTU logo
         html.Span(
             "Please cite: Levassor, Whitford, Petersen, Madsen, Blin, Weber, Frandsen; TBA journal 2024 ",
             style={"fontSize": "18px", "color": "#ddd"}
@@ -14,7 +14,7 @@ footer_content = html.Footer([
             href="https://doi.org/TBA",
             style={"fontSize": "18px", "color": "#00c39a", "marginLeft": "10px"}
         ),
-        html.Img(src="/assets/cfb_logo.png", style={"height": "50px", "marginLeft": "20px"})  # CRB logo
+        html.Img(src="/assets/cfb_logo.png", style={"height": "100px", "marginLeft": "20px"})  # CRB logo
     ], style={"textAlign": "center", "padding": "20px 0", "backgroundColor": "#1A242F", "color": "#ddd", "fontSize": "30px"}),  # Center-align text, add padding, set background and text colors, and increase font size
     html.Div([
         html.A("Contact Us", href="mailto:your-email@example.com", style={"marginRight": "20px", "fontSize": "18px", "color": "#ddd"}),  # Email link
