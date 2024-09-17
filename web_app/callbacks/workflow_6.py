e774781482e4e7b781a851eab3229260dabe530a
Lucas Levassor <lucaslevassor@gmail.com>
2024-09-17 14:07:29 +0200

    wip

--- web_app/callbacks/workflow_6.py
+++ web_app/callbacks/workflow_6.py
@@ -17,7 +17,6 @@ from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 
 from dash.exceptions import PreventUpdate
-from Bio.Restriction import NcoI
 
 
 # Local module imports
