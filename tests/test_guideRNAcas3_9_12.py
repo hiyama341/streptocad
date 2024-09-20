d76e08c9c7116f8b24f82dbd4e6d26bf0cffad3e
Lucas Levassor <lucaslevassor@gmail.com>
2024-09-20 14:29:27 +0200

    wip: freeze python version adn add tolerance to pd.testing

--- tests/test_guideRNAcas3_9_12.py
+++ tests/test_guideRNAcas3_9_12.py
@@ -276,7 +276,11 @@ def test_extract_sgRNAs_output(sgrna_args_cas9, expected_sgrna_df):
     # Check that the DataFrame content matches the expected content
     print(sgrna_df)
     print(expected_sgrna_df)
-    pd.testing.assert_frame_equal(sgrna_df.reset_index(drop=True), expected_sgrna_df.reset_index(drop=True))
+    pd.testing.assert_frame_equal(
+        sgrna_df.reset_index(drop=True),
+        expected_sgrna_df.reset_index(drop=True),
+        atol=1e-5
+    )
 
     # Additional specific checks (optional)
     assert (sgrna_df['gc'] >= sgrna_args_cas9.gc_lower).all() and (sgrna_df['gc'] <= sgrna_args_cas9.gc_upper).all(), "GC content is out of bounds"
@@ -301,8 +305,11 @@ def test_extract_sgRNAs_cas9_basic(sgrna_args_cas9, expected_sgrna_df):
     assert sgrna_df['off_target_count'].is_monotonic_increasing, "The DataFrame should be sorted by off-target counts in ascending order"
 
     # Compare with the expected DataFrame loaded from CSV
-    pd.testing.assert_frame_equal(sgrna_df.reset_index(drop=True), expected_sgrna_df.reset_index(drop=True), 
-                                  check_like=True)
+    pd.testing.assert_frame_equal(
+        sgrna_df.reset_index(drop=True),
+        expected_sgrna_df.reset_index(drop=True),
+        atol=1e-5
+    )
     print(sgrna_df)
     print(expected_sgrna_df)
 
@@ -361,8 +368,11 @@ def test_extract_sgRNAs_cas3_basic(sgrna_args_cas3, expected_cas3_sgrna_df):
     assert sgrna_df['off_target_count'].is_monotonic_increasing, "The DataFrame should be sorted by off-target counts in ascending order"
 
     # Compare with the expected DataFrame loaded from CSV
-    pd.testing.assert_frame_equal(sgrna_df.reset_index(drop=True), expected_cas3_sgrna_df.reset_index(drop=True), 
-                                  check_like=True)
+    pd.testing.assert_frame_equal(
+        sgrna_df.reset_index(drop=True),
+        expected_sgrna_df.reset_index(drop=True),
+        atol=1e-5
+    )
     
     print(sgrna_df)
     print(expected_cas3_sgrna_df)
@@ -416,8 +426,11 @@ def test_extract_sgRNAs_output_cas3(sgrna_args_cas3, expected_cas3_sgrna_df):
     assert len(sgrna_df) == len(expected_cas3_sgrna_df), f"The number of rows ({len(sgrna_df)}) does not match the expected ({len(expected_cas3_sgrna_df)})"
 
     # Check that the DataFrame content matches the expected content
-    pd.testing.assert_frame_equal(sgrna_df.reset_index(drop=True), expected_cas3_sgrna_df.reset_index(drop=True))
-
+    pd.testing.assert_frame_equal(
+        sgrna_df.reset_index(drop=True),
+        expected_sgrna_df.reset_index(drop=True),
+        atol=1e-5
+    )
     print(sgrna_df)
     print(expected_cas3_sgrna_df)
 
