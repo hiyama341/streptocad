bd35e1b05d46782cbc4d658737b8e6988b9ab623
Lucas Levassor <lucaslevassor@gmail.com>
2024-10-11 11:37:43 +0200

    wip: workflow4 has tooltips incorporated

--- web_app/tabs/workflow_4_tab.py
+++ web_app/tabs/workflow_4_tab.py
@@ -5,11 +5,11 @@ from dash.dash_table import DataTable
 from styling import text_style, upload_button_style, card_style, link_style, table_style
 from streptocad.utils import polymerase_dict
 from components import upload_component_with_display
-
+from tooltip import create_tooltip, tooltips  # Import the tooltips
 
 # Reference content used in multiple tabs
 reference_content = html.Div([
-    html.P("Note: For more information on CRISPR techniques and details, please visit the Nature protocols article below (the figure and protocol is from there)."),
+    html.P("Note: For more information on CRISPR techniques and details, please visit the Nature protocols article below."),
     html.A("CRISPR–Cas9, CRISPRi and CRISPR-BEST-mediated genetic manipulation in streptomycetes", href="https://www.nature.com/articles/s41596-020-0339-z", target="_blank"),
 ], style=text_style)
 
@@ -38,8 +38,7 @@ crispri_tab = html.Div(children=[
         ], style={'padding': '20px', 'backgroundColor': '#2C3E50'}),
     ], className="mb-4"),
 
-    html.Img(src='/assets/w4_pic.png', 
-             style={'width': '50%', 'margin': '20px auto'}),
+    html.Img(src='/assets/w4_pic.png', style={'width': '50%', 'margin': '20px auto'}),
 
     dbc.Row([
         dbc.Col(
@@ -60,7 +59,6 @@ crispri_tab = html.Div(children=[
         )
     ], className="mb-4", justify="start"),
 
-    
     # UPLOAD genome
     dbc.Row([
         dbc.Col([
@@ -69,7 +67,7 @@ crispri_tab = html.Div(children=[
                 dbc.CardBody([
                     html.H5("Genome File (GenBank format)", className="card-title", style=text_style),
                     upload_component_with_display(
-                        {'type': 'upload-component', 'index': 'genome-file-4'},  # Corrected: Directly use the dictionary
+                        {'type': 'upload-component', 'index': 'genome-file-4'},
                         text_style, 
                         link_style, 
                         upload_button_style
@@ -86,7 +84,7 @@ crispri_tab = html.Div(children=[
                 dbc.CardBody([
                     html.H5("CRISPRi plasmid (GenBank format)", className="card-title", style=text_style),
                     upload_component_with_display(
-                        {'type': 'upload-component', 'index': 'single-vector-4'},  # Corrected: Directly use the dictionary
+                        {'type': 'upload-component', 'index': 'single-vector-4'},
                         text_style, 
                         link_style, 
                         upload_button_style
@@ -124,7 +122,7 @@ crispri_tab = html.Div(children=[
 
     dbc.Row([
         dbc.Col([
-            html.Label('5 prime Overhang:', style=text_style),
+            html.Label(["5 prime Overhang: ", html.Span("ⓘ", id="forward-overhang-tooltip-4", style=link_style)], style=text_style),
             dbc.Input(
                 id='forward-overhang-input_4',
                 type='text',
@@ -132,11 +130,12 @@ crispri_tab = html.Div(children=[
                 value='CGGTTGGTAGGATCGACGGC'
             ),
         ], width=6, style={"marginRight": "10px", "marginLeft": "10px"}),
+        create_tooltip("Enter the 5' overhang sequence for oligo nucleotide synthesis.", "forward-overhang-tooltip-4"),
     ]),
 
     dbc.Row([
         dbc.Col([
-            html.Label('3 prime Overhang:', style=text_style),
+            html.Label(["3 prime Overhang: ", html.Span("ⓘ", id="reverse-overhang-tooltip-4", style=link_style)], style=text_style),
             dbc.Input(
                 id='reverse-overhang-input_4',
                 type='text',
@@ -144,6 +143,7 @@ crispri_tab = html.Div(children=[
                 value='GTTTTAGAGCTAGAAATAGC'
             ),
         ], width=6, style={"marginRight": "10px", "marginLeft": "10px"}),
+        create_tooltip("Enter the 3' overhang sequence for oligo nucleotide synthesis.", "reverse-overhang-tooltip-4"),
     ], className="mb-5"),
     
     dbc.Row([
@@ -151,43 +151,51 @@ crispri_tab = html.Div(children=[
             html.H4("5) Filtering metrics for sgRNAs", style=text_style),
             dbc.Row([
                 dbc.Col([
-                    dbc.Label("GC Content Upper Bound", style={'color': '#ddd'}),  
+                    dbc.Label(["GC Content Upper Bound ", html.Span("ⓘ", id="gc-upper-tooltip-4", style=link_style)], style={'color': '#ddd'}),  
                     dbc.Input(
                         id='gc-upper_4',
                         type='number',
                         value=0.99,
                         style={'color': '#000', 'width': '100%'}
                     ),
+                    create_tooltip("Set the upper bound for GC content in sgRNA sequences.", "gc-upper-tooltip-4")
                 ], width=12, className="mb-3"),
+
                 dbc.Col([
-                    dbc.Label("GC Content Lower Bound", style={'color': '#ddd'}),  
+                    dbc.Label(["GC Content Lower Bound ", html.Span("ⓘ", id="gc-lower-tooltip-4", style=link_style)], style={'color': '#ddd'}),  
                     dbc.Input(
                         id='gc-lower_4',
                         type='number',
                         value=0.01,
                         style={'color': '#000', 'width': '100%'}
                     ),
+                    create_tooltip("Set the lower bound for GC content in sgRNA sequences.", "gc-lower-tooltip-4")
                 ], width=12, className="mb-3"),
+
                 dbc.Col([
-                    dbc.Label("Off-Target Seed Length", style={'color': '#ddd'}),  
+                    dbc.Label(["Off-Target Seed Length ", html.Span("ⓘ", id="off-target-seed-tooltip-4", style=link_style)], style={'color': '#ddd'}),  
                     dbc.Input(
                         id='off-target-seed_4',
                         type='number',
                         value=13,
                         style={'color': '#000', 'width': '100%'}
                     ),
+                    create_tooltip("Specify the length of the seed sequence for off-target filtering.", "off-target-seed-tooltip-4")
                 ], width=12, className="mb-3"),
+
                 dbc.Col([
-                    dbc.Label("Off-Target Upper Bound", style={'color': '#ddd'}),  
+                    dbc.Label(["Off-Target Upper Bound ", html.Span("ⓘ", id="off-target-upper-tooltip-4", style=link_style)], style={'color': '#ddd'}),  
                     dbc.Input(
                         id='off-target-upper_4',
                         type='number',
                         value=10,
                         style={'color': '#000', 'width': '100%'}
                     ),
+                    create_tooltip("Set the maximum allowed off-target score for sgRNA filtering.", "off-target-upper-tooltip-4")
                 ], width=12, className="mb-3"),
+
                 dbc.Col([
-                    dbc.Label("Cas Type", style={'color': '#ddd'}),  
+                    dbc.Label(["Cas Type ", html.Span("ⓘ", id="cas-type-tooltip-4", style=link_style)], style={'color': '#ddd'}),  
                     dcc.Dropdown(
                         id='cas-type_4',
                         options=[
@@ -196,24 +204,29 @@ crispri_tab = html.Div(children=[
                         value='cas9',
                         style={'color': '#000', 'width': '100%'}
                     ),
+                    create_tooltip("Select the Cas protein type for the genome editing workflow.", "cas-type-tooltip-4")
                 ], width=12, className="mb-3"),
+
                 dbc.Col([
-                    dbc.Label("Number of sgRNAs per region/locus tag", style={'color': '#ddd'}),  
+                    dbc.Label(["Number of sgRNAs per region/locus tag ", html.Span("ⓘ", id="number-sgrnas-tooltip-4", style=link_style)], style={'color': '#ddd'}),  
                     dbc.Input(
                         id='number-of-sgRNAs-per-group_4',
                         type='number',
                         value=5,
                         style={'color': '#000', 'width': '100%'}
                     ),
+                    create_tooltip("Define the number of sgRNAs for each targeted region or locus.", "number-sgrnas-tooltip-4")
                 ], width=12, className="mb-3"),
+
                 dbc.Col([
-                    dbc.Label("Extension to Promoter Region", style={'color': '#ddd'}),  
+                    dbc.Label(["Extension to Promoter Region ", html.Span("ⓘ", id="extension-to-promoter-region-tooltip", style=link_style)], style={'color': '#ddd'}),  
                     dbc.Input(
                         id='extension-to-promoter-region_4',
                         type='number',
                         value=100,
                         style={'color': '#000', 'width': '100%'}
                     ),
+                    create_tooltip("Set the length for promoter region extension in gene silencing.", "extension-to-promoter-region-tooltip")
                 ], width=12, className="mb-3"),
             ]),
         ], width=6, className="mb-4"),
@@ -225,19 +238,18 @@ crispri_tab = html.Div(children=[
         ], width=12),
     ], className="mb-4"),
     
-
     ### Output
     dbc.Row([
         dbc.Col([
             dcc.Loading(
                 id="loading-overlay-4",
                 type="circle",
+                style={'height': '80px', 'width': '80px', 'margin': 'auto'},  # Adjust size as needed
                 children=[
                     dbc.Card([
                         dbc.CardBody([
                             html.H5("Filtered sgRNAs", className="card-title", style=text_style),
                             DataTable(id='mutated-sgrna-table_4', **table_style),
-                            # Removed the download button
                         ])
                     ], style=card_style),
 
@@ -245,7 +257,6 @@ crispri_tab = html.Div(children=[
                         dbc.CardBody([
                             html.H5("Primers", className="card-title", style=text_style),
                             DataTable(id='primer-table_4', **table_style),
-                            # Removed the download button
                         ])
                     ], style=card_style),
                     
@@ -263,7 +274,7 @@ crispri_tab = html.Div(children=[
                             html.A(
                                 'Download All Data & protocols',
                                 id='download-data-and-protocols-link_4',
-                                download="all_data.csv",
+                                download="data_package",
                                 href="",
                                 target="_blank",
                                 className="btn btn-primary"
@@ -273,5 +284,8 @@ crispri_tab = html.Div(children=[
                 ]
             )
         ], width=10),
-]),
+    ]),
+
+    # Include all tooltips
+    *tooltips
 ])
