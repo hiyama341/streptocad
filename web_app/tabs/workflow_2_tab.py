8456e8c33dd4cca125527a281d709b965c22b0f5
Lucas Levassor <lucaslevassor@gmail.com>
2024-10-11 10:50:20 +0200

    wip: added tooltips to the workflow

--- web_app/tabs/workflow_2_tab.py
+++ web_app/tabs/workflow_2_tab.py
@@ -2,11 +2,10 @@ from dash import dcc, html
 import dash_bootstrap_components as dbc
 from dash.dash_table import DataTable
 from components import upload_component
-
+from tooltip import create_tooltip, tooltips  # Import the tooltips
 
 from styling import text_style, upload_button_style, card_style, link_style, table_style, table_header_style, table_row_style
 from streptocad.utils import polymerase_dict
-
 
 # Reference content used in multiple tabs
 reference_content = html.Div([
@@ -63,8 +62,6 @@ crispr_cb_tab = html.Div(children=[
     ], className="mb-4", justify="start"),
     
     # UPLOAD genome
-    
-    
     dbc.Row([
         dbc.Col([
             html.H4("1) Upload your genome file", style=text_style),
@@ -72,7 +69,7 @@ crispr_cb_tab = html.Div(children=[
                 dbc.CardBody([
                     html.H5("Genome File (GenBank format)", className="card-title", style=text_style),
                     dcc.Upload(
-                        id={'type': 'upload-component', 'index': 'genome-file-2'},  # Updated to use pattern matching ID
+                        id={'type': 'upload-component', 'index': 'genome-file-2'},
                         children=html.Div([
                             'Drag and Drop or ',
                             html.A('Select Genome File', style=link_style)
@@ -80,7 +77,7 @@ crispr_cb_tab = html.Div(children=[
                         style=upload_button_style,
                         multiple=False
                     ),
-                    html.Div(id={'type': 'filename-display', 'index': 'genome-file-2'}, children=[], style=text_style),  # Updated to use pattern matching ID
+                    html.Div(id={'type': 'filename-display', 'index': 'genome-file-2'}, children=[], style=text_style),
                 ])
             ], style=card_style),
         ], width=6),
@@ -93,7 +90,7 @@ crispr_cb_tab = html.Div(children=[
                 dbc.CardBody([
                     html.H5("CRISPR-BEST plasmid (GenBank format)", className="card-title", style=text_style),
                     dcc.Upload(
-                        id={'type': 'upload-component', 'index': 'single-vector-2'},  # Updated to use pattern matching ID
+                        id={'type': 'upload-component', 'index': 'single-vector-2'},
                         children=html.Div([
                             'Drag and Drop or ',
                             html.A('Select CRISPR-BEST plasmid File', style=link_style)
@@ -101,13 +98,12 @@ crispr_cb_tab = html.Div(children=[
                         style=upload_button_style,
                         multiple=False
                     ),
-                    html.Div(id={'type': 'filename-display', 'index': 'single-vector-2'}, children=[], style=text_style),  # Updated to use pattern matching ID
+                    html.Div(id={'type': 'filename-display', 'index': 'single-vector-2'}, children=[], style=text_style),
                 ])
             ], style=card_style),
         ], width=6),
     ], className="mb-5"),
 
-        
     dbc.Row([
         dbc.Col([
             html.H4("3) Choose genes/regions to edit", style=text_style),
@@ -115,10 +111,9 @@ crispr_cb_tab = html.Div(children=[
                 dbc.CardBody([
                     html.H5("Example for genes: SCO5087, SCO5087,... (comma-separated)", className="card-title", style=text_style),
                     html.H5("Example for regions: 1000-2000, 100000-101000,... (comma-separated)", className="card-title", style=text_style),
-
                     dbc.Textarea(
                         id='genes-to-KO_2',
-                        placeholder='Enter genes to knock out, e.g., SCO5087',
+                        placeholder='Enter genes to edit, e.g., SCO5087',
                         value='SCO5087',
                         style={'width': '100%', 'height': '100px'}
                     )
@@ -137,7 +132,7 @@ crispr_cb_tab = html.Div(children=[
 
     dbc.Row([
         dbc.Col([
-            html.Label('5 prime Overhang:', style=text_style),
+            html.Label(['5 prime Overhang:', html.Span("ⓘ", id="forward-overhang-tooltip", style=link_style)], style=text_style),
             dbc.Input(
                 id='forward-overhang-input_2',
                 type='text',
@@ -149,7 +144,7 @@ crispr_cb_tab = html.Div(children=[
 
     dbc.Row([
         dbc.Col([
-            html.Label('3 prime Overhang:', style=text_style),
+            html.Label(['3 prime Overhang:', html.Span("ⓘ", id="reverse-overhang-tooltip", style=link_style)], style=text_style),
             dbc.Input(
                 id='reverse-overhang-input_2',
                 type='text',
@@ -159,83 +154,80 @@ crispr_cb_tab = html.Div(children=[
         ], width=6, style={"marginRight": "10px", "marginLeft": "10px"}),
     ], className="mb-5"),
 
+    # Filtering metrics for sgRNAs
     dbc.Col([
         html.H4("5) Filtering metrics for sgRNAs", style=text_style),
         dbc.Row([
             dbc.Col([
-                dbc.Label("GC Content Upper Bound", style={'color': '#ddd'}),  # Explicitly set color to ensure visibility
+                dbc.Label(["GC Content Upper Bound", html.Span("ⓘ", id="gc-upper-tooltip", style=link_style)], style={'color': '#ddd'}),
                 dbc.Input(
                     id='gc-upper_2',
                     type='number',
                     value=0.99,
-                    style={'color': '#000', 'width': '100%'}  # Ensure text is black and set width
+                    style={'color': '#000', 'width': '100%'}
                 ),
             ], width=12, className="mb-3"),
         ]),
         dbc.Row([
             dbc.Col([
-                dbc.Label("GC Content Lower Bound", style={'color': '#ddd'}),  # Explicitly set color to ensure visibility
+                dbc.Label(["GC Content Lower Bound", html.Span("ⓘ", id="gc-lower-tooltip", style=link_style)], style={'color': '#ddd'}),
                 dbc.Input(
                     id='gc-lower_2',
                     type='number',
                     value=0.01,
-                    style={'color': '#000', 'width': '100%'}  # Ensure text is black and set width
+                    style={'color': '#000', 'width': '100%'}
                 ),
             ], width=12, className="mb-3"),
         ]),
         dbc.Row([
             dbc.Col([
-                dbc.Label("Off-Target Seed Length", style={'color': '#ddd'}),  # Explicitly set color to ensure visibility
+                dbc.Label(["Off-Target Seed Length", html.Span("ⓘ", id="off-target-seed-tooltip", style=link_style)], style={'color': '#ddd'}),
                 dbc.Input(
                     id='off-target-seed_2',
                     type='number',
                     value=13,
-                    style={'color': '#000', 'width': '100%'}  # Ensure text is black and set width
+                    style={'color': '#000', 'width': '100%'}
                 ),
             ], width=12, className="mb-3"),
         ]),
         dbc.Row([
             dbc.Col([
-                dbc.Label("Off-Target Upper Bound", style={'color': '#ddd'}),  # Explicitly set color to ensure visibility
+                dbc.Label(["Off-Target Upper Bound", html.Span("ⓘ", id="off-target-upper-tooltip", style=link_style)], style={'color': '#ddd'}),
                 dbc.Input(
                     id='off-target-upper_2',
                     type='number',
                     value=10,
-                    style={'color': '#000', 'width': '100%'}  # Ensure text is black and set width
+                    style={'color': '#000', 'width': '100%'}
                 ),
             ], width=12, className="mb-3"),
         ]),
         dbc.Row([
             dbc.Col([
-                dbc.Label("Cas Type", style={'color': '#ddd'}),  # Explicitly set color to ensure visibility
+                dbc.Label(["Cas Type", html.Span("ⓘ", id="cas-type-tooltip", style=link_style)], style={'color': '#ddd'}),
                 dcc.Dropdown(
                     id='cas-type_2',
-                    options=[
-                        {'label': 'Cas9', 'value': 'cas9'},
-                    ],
+                    options=[{'label': 'Cas9', 'value': 'cas9'}],
                     value='cas9',
-                    style={'color': '#000', 'width': '100%'}  # Ensure text is black and set width
+                    style={'color': '#000', 'width': '100%'}
                 ),
             ], width=12, className="mb-3"),
         ]),
         dbc.Row([
             dbc.Col([
-                dbc.Label("Number of sgRNAs per region/locus tag", style={'color': '#ddd'}),  # Explicitly set color to ensure visibility
+                dbc.Label(["Number of sgRNAs per region/locus tag", html.Span("ⓘ", id="number-sgrnas-tooltip", style=link_style)], style={'color': '#ddd'}),
                 dbc.Input(
                     id='number-of-sgRNAs-per-group_2',
                     type='number',
                     value=5,
-                    style={'color': '#000', 'width': '100%'}  # Ensure text is black and set width
+                    style={'color': '#000', 'width': '100%'}
                 ),
             ], width=12, className="mb-3"),
         ]),
         dbc.Row([
             dbc.Col([
                 dbc.Checklist(
-                    options=[
-                        {"label": "Only Stop Codons", "value": 1},
-                    ],
-                    value=[1] if True else [],  # default value
+                    options=[{"label": "Only Stop Codons", "value": 1}],
+                    value=[1],
                     id="only-stop-codons-checkbox_2",
                     inline=True,
                     switch=True,
@@ -246,10 +238,8 @@ crispr_cb_tab = html.Div(children=[
         dbc.Row([
             dbc.Col([
                 dbc.Checklist(
-                    options=[
-                        {"label": "Editing Sequence Context", "value": 1},
-                    ],
-                    value=[1] if True else [],  # default value
+                    options=[{"label": "Editing Sequence Context", "value": 1}],
+                    value=[1],
                     id="editing_context_2",
                     inline=True,
                     switch=True,
@@ -259,15 +249,12 @@ crispr_cb_tab = html.Div(children=[
         ]),
     ], width=6, style={"marginRight": "10px", "marginLeft": "10px"}),
 
-
-
+    # Advanced settings
     dbc.Row([
         dbc.Col([
             html.H4("7) Show advanced settings for checking primers", style=text_style),
             dbc.Checklist(
-                options=[
-                    {"label": "", "value": 1},
-                ],
+                options=[{"label": "", "value": 1}],
                 value=[],
                 id="show-advanced-settings-checkbox",
                 inline=True,
@@ -282,15 +269,7 @@ crispr_cb_tab = html.Div(children=[
             html.Div(id='advanced-settings-container', children=[
                 dbc.Row([
                     dbc.Col([
-                        dbc.Label([
-                            "Choose Polymerase ",
-                            html.Span("ⓘ", id="polymerase-tooltip-icon-2", style=link_style)
-                        ], style={'color': '#ddd'}),
-                        dbc.Tooltip(
-                            "Select the polymerase type. The default is optimized for high fidelity.",
-                            target="polymerase-tooltip-icon-2",
-                            placement="top",
-                        ),
+                        dbc.Label(["Choose Polymerase", html.Span("ⓘ", id="polymerase-tooltip", style=link_style)], style={'color': '#ddd'}),
                         dcc.Dropdown(
                             id='chosen-polymerase_2',
                             options=dropdown_options,
@@ -298,17 +277,9 @@ crispr_cb_tab = html.Div(children=[
                             style={'color': '#000'}
                         ),
                     ], width=6),
-                    
+
                     dbc.Col([
-                        dbc.Label([
-                            "Target Melting Temperature (°C) ",
-                            html.Span("ⓘ", id="melting-temp-tooltip-icon-2", style=link_style)
-                        ], style={'color': '#ddd'}),
-                        dbc.Tooltip(
-                            "Set the desired melting temperature for the PCR reactions.",
-                            target="melting-temp-tooltip-icon-2",
-                            placement="top",
-                        ),
+                        dbc.Label(["Target Melting Temperature (°C)", html.Span("ⓘ", id="melting-temp-tooltip", style=link_style)], style={'color': '#ddd'}),
                         dbc.Input(
                             id='melting-temperature_2',
                             type='number',
@@ -318,15 +289,7 @@ crispr_cb_tab = html.Div(children=[
                     ], width=6),
 
                     dbc.Col([
-                        dbc.Label([
-                            "Primer Concentration (μM) ",
-                            html.Span("ⓘ", id="primer-concentration-tooltip-icon-2", style=link_style)
-                        ], style={'color': '#ddd'}),
-                        dbc.Tooltip(
-                            "Specify the concentration of primers used in the PCR reaction.",
-                            target="primer-concentration-tooltip-icon-2",
-                            placement="top",
-                        ),
+                        dbc.Label(["Primer Concentration (μM)", html.Span("ⓘ", id="primer-concentration-tooltip", style=link_style)], style={'color': '#ddd'}),
                         dbc.Input(
                             id='primer-concentration_2',
                             type='number',
@@ -336,15 +299,7 @@ crispr_cb_tab = html.Div(children=[
                     ], width=6),
 
                     dbc.Col([
-                        dbc.Label([
-                            "Primer Number Increment ",
-                            html.Span("ⓘ", id="primer-increment-tooltip-icon-2", style=link_style)
-                        ], style={'color': '#ddd'}),
-                        dbc.Tooltip(
-                            "Choose the increment for primer numbering in the workflow.",
-                            target="primer-increment-tooltip-icon-2",
-                            placement="top",
-                        ),
+                        dbc.Label(["Primer Number Increment", html.Span("ⓘ", id="primer-increment-tooltip", style=link_style)], style={'color': '#ddd'}),
                         dbc.Input(
                             id='primer-number-increment_2',
                             type='number',
@@ -354,15 +309,7 @@ crispr_cb_tab = html.Div(children=[
                     ], width=6),
 
                     dbc.Col([
-                        dbc.Label([
-                            "Flanking Region Number ",
-                            html.Span("ⓘ", id="flanking-region-tooltip-icon-2", style=link_style)
-                        ], style={'color': '#ddd'}),
-                        dbc.Tooltip(
-                            "Set the size of the flanking regions for your PCR reactions.",
-                            target="flanking-region-tooltip-icon-2",
-                            placement="top",
-                        ),
+                        dbc.Label(["Flanking Region Number", html.Span("ⓘ", id="flanking-region-tooltip", style=link_style)], style={'color': '#ddd'}),
                         dbc.Input(
                             id='flanking-region-number_2',
                             type='number',
@@ -370,13 +317,12 @@ crispr_cb_tab = html.Div(children=[
                             style={'color': '#000'}
                         ),
                     ], width=6),
-                    
                 ])
-            ], style={"display": "none"})  # Hidden by default
+            ], style={"display": "none"})
         ], width=6),
     ], className="mb-4"),
 
-
+    # Submit Button
     dbc.Row([
         dbc.Col([
             dbc.Button('Submit', id='submit-settings-button_2', color="primary", className="mt-3"),
@@ -437,5 +383,6 @@ crispr_cb_tab = html.Div(children=[
         ], width=10),
     ]),
 
+    # Include the tooltips at the end of the layout
+    *tooltips
 ])
-
