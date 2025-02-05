78f1f44e0e50a18a3610df7c4062ef122ab5a702
Lucas Levassor <lucaslevassor@gmail.com>
2025-02-05 14:14:34 +0100

    wip: new tab icon

--- web_app/application.py
+++ web_app/application.py
@@ -2,6 +2,7 @@
 # MIT License
 
 import os
+
 # DASH imports
 import dash
 from dash import dcc, html, no_update
@@ -14,17 +15,33 @@ from urllib.parse import quote
 import dash_bootstrap_components as dbc
 
 # Styling module
-from styling import tab_style, active_tab_style, vertical_tab_style, text_style, upload_button_style, card_style, link_style, button_style_darkly, table_style
+from styling import (
+    tab_style,
+    active_tab_style,
+    vertical_tab_style,
+    text_style,
+    upload_button_style,
+    card_style,
+    link_style,
+    button_style_darkly,
+    table_style,
+)
 
 # Import callback registration functions
-from callbacks.workflow_1 import register_workflow_1_callbacks  # Import the new callback registration function
+from callbacks.workflow_1 import (
+    register_workflow_1_callbacks,
+)  # Import the new callback registration function
 from callbacks.workflow_2 import register_workflow_2_callbacks
 from callbacks.workflow_3 import register_workflow_3_callbacks
-from callbacks.workflow_4 import register_workflow_4_callbacks  # Added callback registration for workflow 4
+from callbacks.workflow_4 import (
+    register_workflow_4_callbacks,
+)  # Added callback registration for workflow 4
 from callbacks.workflow_5 import register_workflow_5_callbacks
 from callbacks.workflow_6 import register_workflow_6_callbacks
 
-from callbacks_interactivity import register_interactivity_callbacks  # Import the interactivity callbacks
+from callbacks_interactivity import (
+    register_interactivity_callbacks,
+)  # Import the interactivity callbacks
 
 # Import tab content
 from tabs.workflow_1_tab import workflow_1_tab
@@ -43,83 +60,143 @@ from about_and_misson_statement import about_streptocad_and_mission_statement
 
 external_stylesheets = [
     dbc.themes.DARKLY,
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css',
-    '/assets/style.css',  # Include custom CSS for styling switches
-    '/assets/custom_style.css'  # Include custom CSS for the popup
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css",
+    "/assets/style.css",  # Include custom CSS for styling switches
+    "/assets/custom_style.css",  # Include custom CSS for the popup
 ]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
+app = dash.Dash(
+    __name__,
+    external_stylesheets=external_stylesheets,
+    suppress_callback_exceptions=True,
+)
+app.title = "StreptoCAD"  # This changes the browser tab title
+
+
+# Customize the index_string to use your ICO file as the favicon.
+app.index_string = """
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>{%title%}</title>
+        <!-- Use your custom favicon file -->
+        <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+"""
+
 # For AWS
 application = app.server
 
 # Main VERTICAL app layout with the loading spinner for submit button
-main_layout = dbc.Container([
-    header_content,
-    dbc.Row([
-        # Main Tabs on the left side
-        dbc.Col([
-            html.H2("Workflows", style={'color': '#ddd', 'padding': '20px 0'}),  # Add a heading
-            dcc.Tabs(id="main-tabs", vertical=True, style={'borderRight': '0.1px solid black'}, children=[
-                dcc.Tab(label="Workflow 1: Overexpression library construction", value="workflow_1", style=vertical_tab_style, selected_style=active_tab_style),
-                dcc.Tab(label="Workflow 2: Single CRISPR-BEST plasmid generation", value="workflow_2", style=vertical_tab_style, selected_style=active_tab_style),
-                dcc.Tab(label="Workflow 3: Multiplexed CRISPR-BEST plasmid generation", value="workflow_3", style=vertical_tab_style, selected_style=active_tab_style),
-                dcc.Tab(label="Workflow 4: CRISPRi plasmid generation", value="workflow_4", style=vertical_tab_style, selected_style=active_tab_style),
-                dcc.Tab(label="Workflow 5: CRISPR-Cas9 plasmid generation", value="workflow_5", style=vertical_tab_style, selected_style=active_tab_style),
-                dcc.Tab(label="Workflow 6: CRISPR-Cas3 plasmid generation", value="workflow_6", style=vertical_tab_style, selected_style=active_tab_style),
-                dcc.Tab(label="About StreptoCAD & Mission Statement", value="about", style=vertical_tab_style, selected_style=active_tab_style)
-            ])
-        ], width=3),
-        dbc.Col([
-            html.Div(id="tab-content", style={'padding': '20px'})
-        ], width=9),
-    ]),
-    footer_content,
-
-    #### ERROR BOX 
-    dcc.ConfirmDialog(
-        id='error-dialog_1',
-        message=""
-    ),
-    dcc.ConfirmDialog(
-        id='error-dialog_2',
-        message=""
-    ),
-    dcc.ConfirmDialog(
-        id='error-dialog_3',
-        message=""
-    ),
-    dcc.ConfirmDialog(
-        id='error-dialog_4',
-        message=""
-    ),
-    dcc.ConfirmDialog(
-        id='error-dialog_5',
-        message=""
-    ),
-    dcc.ConfirmDialog(
-        id='error-dialog_6',
-        message=""
-    ),
-], fluid=True, style={'backgroundColor': '#2C3E50', 'padding': '20px', 'color': '#ddd'})
+main_layout = dbc.Container(
+    [
+        header_content,
+        dbc.Row(
+            [
+                # Main Tabs on the left side
+                dbc.Col(
+                    [
+                        html.H2(
+                            "Workflows", style={"color": "#ddd", "padding": "20px 0"}
+                        ),  # Add a heading
+                        dcc.Tabs(
+                            id="main-tabs",
+                            vertical=True,
+                            style={"borderRight": "0.1px solid black"},
+                            children=[
+                                dcc.Tab(
+                                    label="Workflow 1: Overexpression library construction",
+                                    value="workflow_1",
+                                    style=vertical_tab_style,
+                                    selected_style=active_tab_style,
+                                ),
+                                dcc.Tab(
+                                    label="Workflow 2: Single CRISPR-BEST plasmid generation",
+                                    value="workflow_2",
+                                    style=vertical_tab_style,
+                                    selected_style=active_tab_style,
+                                ),
+                                dcc.Tab(
+                                    label="Workflow 3: Multiplexed CRISPR-BEST plasmid generation",
+                                    value="workflow_3",
+                                    style=vertical_tab_style,
+                                    selected_style=active_tab_style,
+                                ),
+                                dcc.Tab(
+                                    label="Workflow 4: CRISPRi plasmid generation",
+                                    value="workflow_4",
+                                    style=vertical_tab_style,
+                                    selected_style=active_tab_style,
+                                ),
+                                dcc.Tab(
+                                    label="Workflow 5: CRISPR-Cas9 plasmid generation",
+                                    value="workflow_5",
+                                    style=vertical_tab_style,
+                                    selected_style=active_tab_style,
+                                ),
+                                dcc.Tab(
+                                    label="Workflow 6: CRISPR-Cas3 plasmid generation",
+                                    value="workflow_6",
+                                    style=vertical_tab_style,
+                                    selected_style=active_tab_style,
+                                ),
+                                dcc.Tab(
+                                    label="About StreptoCAD & Mission Statement",
+                                    value="about",
+                                    style=vertical_tab_style,
+                                    selected_style=active_tab_style,
+                                ),
+                            ],
+                        ),
+                    ],
+                    width=3,
+                ),
+                dbc.Col(
+                    [html.Div(id="tab-content", style={"padding": "20px"})], width=9
+                ),
+            ]
+        ),
+        footer_content,
+        #### ERROR BOX
+        dcc.ConfirmDialog(id="error-dialog_1", message=""),
+        dcc.ConfirmDialog(id="error-dialog_2", message=""),
+        dcc.ConfirmDialog(id="error-dialog_3", message=""),
+        dcc.ConfirmDialog(id="error-dialog_4", message=""),
+        dcc.ConfirmDialog(id="error-dialog_5", message=""),
+        dcc.ConfirmDialog(id="error-dialog_6", message=""),
+    ],
+    fluid=True,
+    style={"backgroundColor": "#2C3E50", "padding": "20px", "color": "#ddd"},
+)
 
 # Overall layout with introduction page
 app.layout = html.Div(id="page-content", children=[introduction_page])
+
 
 # Callbacks
 @app.callback(
     Output("page-content", "children"),
     [Input("start-button", "n_clicks")],
-    [State("page-content", "children")]
+    [State("page-content", "children")],
 )
 def display_main_page(n_clicks, children):
     if n_clicks:
         return main_layout
     return children
 
-@app.callback(
-    Output("tab-content", "children"),
-    [Input("main-tabs", "value")]
-)
+
+@app.callback(Output("tab-content", "children"), [Input("main-tabs", "value")])
 def render_tab_content(tab):
     if tab == "about":
         return about_streptocad_and_mission_statement
@@ -138,9 +215,9 @@ def render_tab_content(tab):
     else:
         return welcome_message_content
 
+
 @app.callback(
-    Output("submit-output", "children"),
-    [Input("submit-settings-button_1", "n_clicks")]
+    Output("submit-output", "children"), [Input("submit-settings-button_1", "n_clicks")]
 )
 def render_loading_spinner(n_clicks):
     if n_clicks:
@@ -149,20 +226,22 @@ def render_loading_spinner(n_clicks):
         return f"Process completed {n_clicks} times."
     return no_update
 
+
 # Callback to show/hide advanced settings
 @app.callback(
-    Output('advanced-settings-container', 'style'),
-    [Input('show-advanced-settings-checkbox', 'value')]
+    Output("advanced-settings-container", "style"),
+    [Input("show-advanced-settings-checkbox", "value")],
 )
 def toggle_advanced_settings(checkbox_value):
     if checkbox_value:
-        return {'display': 'block'}
+        return {"display": "block"}
     else:
-        return {'display': 'none'}
-    
+        return {"display": "none"}
+
+
 @app.callback(
-    Output({'type': 'filename-display', 'index': MATCH}, 'children'),
-    Input({'type': 'upload-component', 'index': MATCH}, 'filename')
+    Output({"type": "filename-display", "index": MATCH}, "children"),
+    Input({"type": "upload-component", "index": MATCH}, "filename"),
 )
 def update_filename_display(filename):
     return f"Uploaded file: {filename}" if filename else "No file uploaded"
@@ -177,17 +256,18 @@ register_workflow_5_callbacks(app)
 register_workflow_6_callbacks(app)
 register_interactivity_callbacks(app)  # Register the interactivity callbacks
 
+
 # Health check endpoint for Docker
-@app.server.route('/health-check')
+@app.server.route("/health-check")
 def health_check():
-    return 'Health Check OK', 200
+    return "Health Check OK", 200
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Check if running in Docker
-    if os.environ.get('DOCKER_CONTAINER'):
+    if os.environ.get("DOCKER_CONTAINER"):
         # Docker environment
-        app.run_server(host='0.0.0.0', port=8050, debug=False)
+        app.run_server(host="0.0.0.0", port=8050, debug=False)
     else:
         # Local development
         app.run_server(debug=True)
